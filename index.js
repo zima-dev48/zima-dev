1bff6a5693e23d99c94acd9db89a5ad9869146d7
zima-dev48 <heisenaltic@gmail.com>
2025-02-23 19:51:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🧸 /zima", "🧸 /zima"];
+const statusMessages = ["/zima", "/zima"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
