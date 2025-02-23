8754f932960eb7df975890e08f4b85ed2ad65b7d
zima-dev48 <heisenaltic@gmail.com>
2025-02-23 19:41:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🧸 /zima", "🧸 /zima"];
 const statusTypes = [ 'dnd', 'idle'];
-let currentStatusIndex = 0;
-let currentTypeIndex = 0;
+let currentStatusIndex = dnd;
+let currentTypeIndex = dnd;
 
 async function login() {
   try {
